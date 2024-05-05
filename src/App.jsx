1b95388a60dc678bc003b2f344be2c99b5e1adf2
Данил <danikkach666@gmail.com>
2024-05-05 11:07:26 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import Home from 'pages/Home';
+import Rates from 'pages/Rates';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      const crd = pos.coords;
+
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+  return (
+    <Routes>
+      <Route element={<Header />} path="/">
+        <Route element={<Home />} index />
+        <Route element={<Rates />} path="/rates" />
+        <Route element={<Navigate to={'/'} replace />} path="*" />
+      </Route>
+    </Routes>
+  );
 };
